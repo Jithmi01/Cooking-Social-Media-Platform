887e910fa15d7e945468f88049911ea0e0f72382
Kavindu Madhusanka <127321810+it22639080@users.noreply.github.com>
2025-04-28 16:59:16 +0530

    Make a group post controller
    make routes in get all, get a specific group post

---
+++ src/main/java/com/example/cookingsystem/controllers/GroupPostController.java
@@ -0,0 +1,60 @@
+package com.example.cookingsystem.controllers;
+
+import com.example.cookingsystem.dtos.GroupPostDTO;
+import com.example.cookingsystem.services.GroupPostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/group-posts")
+public class GroupPostController {
+
+    private final GroupPostService groupPostService;
+
+    @Autowired
+    public GroupPostController(GroupPostService groupPostService) {
+        this.groupPostService = groupPostService;
+    }
+
+    /**
+     * Get all group posts
+     */
+    @GetMapping
+    public ResponseEntity<List<GroupPostDTO.GroupPostResponse>> getAllGroupPosts() {
+        List<GroupPostDTO.GroupPostResponse> posts = groupPostService.getAllGroupPosts();
+        return ResponseEntity.ok(posts);
+    }
+
+    /**
+     * Get a specific group post by ID
+     */
+    @GetMapping("/{postId}")
+    public ResponseEntity<GroupPostDTO.GroupPostResponse> getGroupPostById(@PathVariable String postId) {
+        GroupPostDTO.GroupPostResponse post = groupPostService.getGroupPostById(postId);
+        return ResponseEntity.ok(post);
+    }
+
+    /**
+     * Get posts by group ID
+     */
+    @GetMapping("/group/{groupId}")
+    public ResponseEntity<List<GroupPostDTO.GroupPostResponse>> getPostsByGroupId(@PathVariable String groupId) {
+        List<GroupPostDTO.GroupPostResponse> posts = groupPostService.getPostsByGroupId(groupId);
+        return ResponseEntity.ok(posts);
+    }
+
+    /**
+     * Get posts by user ID
+     */
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<GroupPostDTO.GroupPostResponse>> getPostsByUserId(@PathVariable String userId) {
+        List<GroupPostDTO.GroupPostResponse> posts = groupPostService.getPostsByUserId(userId);
+        return ResponseEntity.ok(posts);
+    }
+
+   
+}
