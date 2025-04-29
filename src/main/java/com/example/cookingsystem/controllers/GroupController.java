b6f1d4ffaeb2c82e402555768157e8b0cdda2ff9
Kavindu Madhusanka <127321810+it22639080@users.noreply.github.com>
2025-04-29 17:02:20 +0530

    change Group controller

--- src/main/java/com/example/cookingsystem/controllers/GroupController.java
+++ src/main/java/com/example/cookingsystem/controllers/GroupController.java
@@ -3,9 +3,14 @@ package com.example.cookingsystem.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -50,4 +55,68 @@ public class GroupController {
         return ResponseEntity.ok(groups);
     }
 
+    /**
+     * Create a new group
+     */
+    @PostMapping
+    public ResponseEntity<GroupDTO.GroupResponse> createGroup(@RequestBody GroupDTO.GroupRequest groupRequest) {
+        GroupDTO.GroupResponse createdGroup = groupService.createGroup(groupRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdGroup);
+    }
+
+    /**
+     * Update an existing group
+     */
+    @PutMapping("/{groupId}")
+    public ResponseEntity<GroupDTO.GroupResponse> updateGroup(
+            @PathVariable String groupId,
+            @RequestBody GroupDTO.GroupRequest groupRequest) {
+        GroupDTO.GroupResponse updatedGroup = groupService.updateGroup(groupId, groupRequest);
+        return ResponseEntity.ok(updatedGroup);
+    }
+
+    /**
+     * Delete a group (soft delete)
+     */
+    @DeleteMapping("/{groupId}")
+    public ResponseEntity<Void> deleteGroup(@PathVariable String groupId) {
+        groupService.deleteGroup(groupId);
+        return ResponseEntity.noContent().build();
+    }
+
+    /**
+     * Add a single user to a group
+     */
+    @PostMapping("/{groupId}/members/{userId}")
+    public ResponseEntity<GroupDTO.GroupResponse> addUserToGroup(
+            @PathVariable String groupId,
+            @PathVariable String userId) {
+        GroupDTO.GroupResponse updatedGroup = groupService.addUserToGroup(groupId, userId);
+        return ResponseEntity.ok(updatedGroup);
+    }
+
+    /**
+     * Add multiple users to a group
+     * {
+     *   "userIds": ["680c5a725ca9ad6a757134c7", "680c60145ca9ad6a757134c8"]
+     * }
+     */
+    @PostMapping("/{groupId}/members")
+    public ResponseEntity<GroupDTO.GroupResponse> addUsersToGroup(
+            @PathVariable String groupId,
+            @RequestBody GroupDTO.AddMembersRequest request) {
+        GroupDTO.GroupResponse updatedGroup = groupService.addUsersToGroup(groupId, request);
+        return ResponseEntity.ok(updatedGroup);
+    }
+
+    /**
+     * Remove a user from a group
+     */
+    @DeleteMapping("/{groupId}/members/{userId}")
+    public ResponseEntity<GroupDTO.GroupResponse> removeUserFromGroup(
+            @PathVariable String groupId,
+            @PathVariable String userId) {
+        GroupDTO.GroupResponse updatedGroup = groupService.removeUserFromGroup(groupId, userId);
+        return ResponseEntity.ok(updatedGroup);
+    }
 }
