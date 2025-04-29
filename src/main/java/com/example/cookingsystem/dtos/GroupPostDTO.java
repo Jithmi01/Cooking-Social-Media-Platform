e9d6a2b1a814d092d61cbfac94887330f6566ba9
Kavindu Madhusanka <127321810+it22639080@users.noreply.github.com>
2025-04-29 09:08:55 +0530

    getter and setter in group post

---
+++ src/main/java/com/example/cookingsystem/dtos/GroupPostDTO.java
@@ -0,0 +1,43 @@
+package com.example.cookingsystem.dtos;
+public class GroupPostDTO {
+
+    public static class GroupPostRequest {
+        private String title;
+        private String description;
+        private String mediaUrl;
+        private String groupId;
+
+        // Getters and setters
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getMediaUrl() {
+            return mediaUrl;
+        }
+
+        public void setMediaUrl(String mediaUrl) {
+            this.mediaUrl = mediaUrl;
+        }
+
+        public String getGroupId() {
+            return groupId;
+        }
+
+        public void setGroupId(String groupId) {
+            this.groupId = groupId;
+        }
+    }
+}
