381cc99e299f5c5f10ef779e9e895456ef4ae2da
SandaniDhammage <sandanid01@gmail.com>
2025-05-10 20:36:58 +0530

    upadate PostFooter.jsx
    make some styled in post footer

--- Frontend/src/components/cookingPosts/PostFooter.jsx
+++ Frontend/src/components/cookingPosts/PostFooter.jsx
@@ -218,6 +218,7 @@ const PostFooter = ({ post }) => {
                 className="flex-grow px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
                 disabled={isLoading}
               />
+              
               <button
                 type="submit"
                 className="bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 disabled:opacity-50"
